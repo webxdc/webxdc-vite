57f3bc7e4519663d425ff8358cc34f155c2f4301
adbenitez <asieldbenitez@gmail.com>
2023-06-23 12:51:46 +0200

    use vite-plugin-zip-pack

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from "vite";
+import zipPack from "vite-plugin-zip-pack";
+
+//@ts-ignore
+import packageJson from "./package.json";
+
+let version = "";
+if (packageJson.version) {
+    version = "_" + packageJson.version;
+}
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [
+        zipPack({
+            outDir: "dist-xdc",
+            outFileName:
+            (packageJson.name || "app") + version + ".xdc",
+        }),
+    ],
+});
