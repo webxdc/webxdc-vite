010188a0ce0f7a003ef76e7556af9d2467643304
adbenitez <asieldbenitez@gmail.com>
2023-06-24 17:00:45 +0200

    apply prettier

--- vite.config.js
+++ vite.config.js
@@ -6,18 +6,17 @@ import packageJson from "./package.json";
 
 let version = "";
 if (packageJson.version) {
-    version = "_" + packageJson.version;
+  version = "_" + packageJson.version;
 }
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [
-        zipPack({
-            outDir: "dist-xdc",
-            outFileName:
-            (packageJson.name || "app") + version + ".xdc",
-        }),
-    ],
+  plugins: [
+    zipPack({
+      outDir: "dist-xdc",
+      outFileName: (packageJson.name || "app") + version + ".xdc",
+    }),
+  ],
 
   // https://github.com/vitest-dev/vitest
   test: {
